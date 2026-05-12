5fafa91a328167767e582820cdcce3635fdb882d
def750 <def75023@gmail.com>
2026-05-12 22:11:30 +0200

    more changes in disks im too lazy to write the commit names ahh

--- kubejs/server_scripts/loottery.js
+++ kubejs/server_scripts/loottery.js
@@ -1,4 +1,6 @@
 // Script to change loot tables for our stuff
+
+// Sharpnel Makiba
 EntityEvents.death((event) => {
   const entity = event.entity;
   const source = String(event.source);
@@ -7,5 +9,23 @@ EntityEvents.death((event) => {
 
   if (entity.type !== "minecraft:cat") return;
 
-  entity.block.popItem("osurooms:makiba");
+  if (Math.random() < 0.001) {
+    entity.block.popItem("osurooms:makiba");
+  }
+});
+
+// Craftable Discs
+ServerEvents.recipes((event) => {
+  event.shaped(
+    Item.of("osurooms:cocodrilo_music", 1), // arg 1: output
+    [
+      "AAA",
+      "ABA", // arg 2: the shape (array of strings)
+      "AAA",
+    ],
+    {
+      A: "minecraft:cocoa_beans",
+      B: "create:mechanical_drill",
+    },
+  );
 });
--- kubejs/startup_scripts/osuroomsradio_items.js
+++ kubejs/startup_scripts/osuroomsradio_items.js
@@ -1,24 +1,24 @@
 const OSUROOMS_DISCS = [
-  { id: 'materialize_anything', texture: 'anubasu_anubasu__materialize_anything' },
-  { id: 'blastix_riotz', texture: 'camellia__blastix_riotz' },
-  { id: 'cocodrilo_music', texture: 'cocodrilo_music' },
-  { id: 'end_time', texture: 'cres__end_time' },
-  { id: 'joe_fight_joe_fight_remix', texture: 'dj_myosuke__joe_fight_joe_fight_remix' },
-  { id: 'daisycutter', texture: 'etia__daisycutter' },
-  { id: 'eden', texture: 'gram__eden' },
-  { id: 'mendes', texture: 'humanoid__mendes' },
-  { id: 'makiba', texture: 'jea_dj_sharpnel__makiba' },
-  { id: 'strong_280', texture: 'katagiri__strong_280' },
-  { id: 'bookmaker', texture: 'kobaryo__bookmaker' },
-  { id: 'dangeroooous_jungle_extended', texture: 'laur__dangeroooous_jungle_extended' },
-  { id: 'bol', texture: 'lv4_feat_iwasaki_chiaki__bol' },
-  { id: 'resonant', texture: 'nekomata_master_and_camellia_w_huer__resonant' },
-  { id: 'podsumowanie_pierwszego_dnia_rozgrywek_grupowych_mp4k', texture: 'podsumowanie_pierwszego_dnia_rozgrywek_grupowych_mp4k' },
-  { id: '666', texture: 'roughsketch__666' },
-  { id: 'far_in_the_blue_sky', texture: 'saikoro__far_in_the_blue_sky' },
-  { id: 'aegleseeker', texture: 'silentroom_vs_frums__aegleseeker' },
-  { id: 'triumph_and_regret', texture: 'typemars__triumph_and_regret' },
-  { id: 'decoy', texture: 'yooh__decoy' },
+  { id: 'materialize_anything', texture: 'anubasu_anubasu__materialize_anything', description: 'anubasu-anubasu - Materialize Anything' },
+  { id: 'blastix_riotz', texture: 'camellia__blastix_riotz', description: 'Camellia - Blastix Riotz' },
+  { id: 'cocodrilo_music', texture: 'cocodrilo_music', description: 'Cocodrilo Music' },
+  { id: 'end_time', texture: 'cres__end_time', description: 'Cres. - End Time' },
+  { id: 'joe_fight_joe_fight_remix', texture: 'dj_myosuke__joe_fight_joe_fight_remix', description: 'Dj Myosuke - Joe Fight (Joe Fight Remix)' },
+  { id: 'daisycutter', texture: 'etia__daisycutter', description: 'ETIA. - Daisycutter' },
+  { id: 'eden', texture: 'gram__eden', description: 'Gram - Eden' },
+  { id: 'mendes', texture: 'humanoid__mendes', description: 'Humanoid - Mendes' },
+  { id: 'makiba', texture: 'jea_dj_sharpnel__makiba', description: 'Jea (DJ Sharpnel) - Makiba' },
+  { id: 'strong_280', texture: 'katagiri__strong_280', description: 'Katagiri - STRONG 280' },
+  { id: 'bookmaker', texture: 'kobaryo__bookmaker', description: 'Kobaryo - Bookmaker' },
+  { id: 'dangeroooous_jungle_extended', texture: 'laur__dangeroooous_jungle_extended', description: 'Laur - DANGEROOOOUS JUNGLE (Extended)' },
+  { id: 'bol', texture: 'lv4_feat_iwasaki_chiaki__bol', description: 'LV.4 feat.岩﨑千明 - B.O.L.' },
+  { id: 'resonant', texture: 'nekomata_master_and_camellia_w_huer__resonant', description: 'Nekomata Master & Camellia w/ "HuΣeR" - Resonant' },
+  { id: 'podsumowanie_pierwszego_dnia_rozgrywek_grupowych_mp4k', texture: 'podsumowanie_pierwszego_dnia_rozgrywek_grupowych_mp4k', description: 'Podsumowanie pierwszego dnia rozgrywek grupowych MP4K' },
+  { id: '666', texture: 'roughsketch__666', description: 'Roughsketch - 666' },
+  { id: 'far_in_the_blue_sky', texture: 'saikoro__far_in_the_blue_sky', description: 'Saikoro - Far in the blue sky' },
+  { id: 'aegleseeker', texture: 'silentroom_vs_frums__aegleseeker', description: 'Silentroom vs Frums - Aegleseeker' },
+  { id: 'triumph_and_regret', texture: 'typemars__triumph_and_regret', description: 'Typemars - Triumph and Regret' },
+  { id: 'decoy', texture: 'yooh__decoy', description: 'Yooh - Decoy' },
 ]
 
 StartupEvents.registry('item', event => {
@@ -26,7 +26,9 @@ StartupEvents.registry('item', event => {
     event.create(`osurooms:${disc.id}`)
       .unstackable()
       .rarity('rare')
-      .texture(`osuroomsradio:item/${disc.texture}`)
+      .texture(`osurooms:item/${disc.id}`)
+      .jukeboxPlayable(`osurooms:${disc.id}`, false)
+      .tooltip(Text.gray(disc.description))
       .tag('minecraft:music_discs')
   }
 })
