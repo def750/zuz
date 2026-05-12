3f5f112de38dc00849b9522c7e750e25b6d0c2d5
def750 <def75023@gmail.com>
2026-05-12 21:19:15 +0200

    quick fix

---
+++ kubejs/server_scripts/loottery.js
@@ -0,0 +1,11 @@
+// Script to change loot tables for our stuff
+EntityEvents.death((event) => {
+  const entity = event.entity;
+  const source = String(event.source);
+
+  if (!source.includes("create.crush")) return;
+
+  if (entity.type !== "minecraft:cat") return;
+
+  entity.block.popItem("osurooms:makiba");
+});
